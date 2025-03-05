0473bc6b006421af4e6482a0a6445c8dad5c6b7b
Sergey Eliseev <sergei.eliseev17@icloud.com>
2025-03-05 14:08:18 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Serge Eliseev on 05.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
